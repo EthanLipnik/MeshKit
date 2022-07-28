9129cf0e48d6c4a9c84d035bf77e8ae0d3a3ec6d
EthanLipnik <hi@ethanlipnik.com>
2022-07-28 15:10:46 -0400

    make grid codable

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
             targets: ["MeshKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/EthanLipnik/MeshGradient.git", from: "1.0.1"),
+        .package(url: "https://github.com/EthanLipnik/MeshGradient.git", from: "1.0.2"),
         .package(url: "https://github.com/EthanLipnik/RandomColorSwift.git", from: "2.0.1")
     ],
     targets: [
