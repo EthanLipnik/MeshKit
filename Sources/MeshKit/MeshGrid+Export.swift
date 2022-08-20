5f14ae90e82f19f73f023f5e144ebd01d48bff6d
Ethan Lipnik <hi@ethanlipnik.com>
2022-08-20 13:28:04 -0400

    improve export options

--- Sources/MeshKit/MeshGrid+Export.swift
+++ Sources/MeshKit/MeshGrid+Export.swift
@@ -17,23 +17,29 @@ extension MeshColorGrid {
 
     @discardableResult
     public func export(to url: URL = FileManager.default.temporaryDirectory.appendingPathComponent(UUID().uuidString + ".png"),
-                       size: MeshPoint = .init(x: 1920, y: 1080),
-                       pixelFormat: MTLPixelFormat = .bgra8Unorm,
-                       colorSpace: CGColorSpace? = .init(name: CGColorSpace.linearSRGB)) async throws -> URL {
+                       size: MeshSize = .init(width: 1920, height: 1080),
+                       subdivisions: Int = MeshDefaults.subdivisions,
+                       grainAlpha: Float = MeshDefaults.grainAlpha,
+                       colorSpace: CGColorSpace? = .init(name: CGColorSpace.sRGB),
+                       fileFormat: UTType = .png,
+                       pixelFormat: MTLPixelFormat = .bgra8Unorm) async throws -> URL {
         return try await withCheckedThrowingContinuation({ continuation in
             let grid = self.asControlPoint()
             let dataProvider = MeshGradientState.static(grid: grid)
                 .createDataProvider()
-            let renderer = MetalMeshRenderer(metalKitView: nil, meshDataProvider: dataProvider, viewportSize: .init(x: size.x, y: size.y), grainAlpha: MeshGradientDefaults.grainAlpha)
+            let renderer = MetalMeshRenderer(metalKitView: nil,
+                                             meshDataProvider: dataProvider,
+                                             viewportSize: .init(x: Float(size.width), y: Float(size.height)),
+                                             grainAlpha: grainAlpha,
+                                             subdivisions: subdivisions)
 
             var metalLayer: CAMetalLayer? = CAMetalLayer()
-
             metalLayer?.colorspace = colorSpace
             metalLayer?.framebufferOnly = false
             metalLayer?.device = MTLCreateSystemDefaultDevice()
             metalLayer?.setNeedsDisplay()
-            metalLayer?.frame = CGRect(origin: .zero, size: CGSize(width: CGFloat(size.x),
-                                                                  height: CGFloat(size.y)))
+            metalLayer?.frame = CGRect(origin: .zero, size: CGSize(width: CGFloat(size.width),
+                                                                  height: CGFloat(size.height)))
             let currentDrwable = metalLayer?.nextDrawable()
 
             let renderPassDescriptor = MTLRenderPassDescriptor()
@@ -46,7 +52,7 @@ extension MeshColorGrid {
                           renderPassDescriptor: renderPassDescriptor,
                           currentDrawable: currentDrwable) { texture in
                 if let texture {
-                    texture.writeTexture(url: url)
+                    texture.writeTexture(url: url, type: fileFormat)
                     continuation.resume(returning: url)
                 } else {
                     continuation.resume(throwing: NSError(domain: "Failed to create texture", code: -1))
@@ -87,7 +93,7 @@ extension MTLTexture {
 
         swizzleBGRA8toRGBA8(imageBytes, width: width, height: height)
 
-        guard let colorSpace = CGColorSpace(name: colorSpace?.name ?? CGColorSpace.linearSRGB) else { return nil }
+        guard let colorSpace = CGColorSpace(name: colorSpace?.name ?? CGColorSpace.sRGB) else { return nil }
         let bitmapInfo = CGImageAlphaInfo.premultipliedLast.rawValue
         guard let bitmapContext = CGContext(data: nil,
                                             width: width,
@@ -114,10 +120,10 @@ extension MTLTexture {
         vImagePermuteChannels_ARGB8888(&sourceBuffer, &destBuffer, &swizzleMask, vImage_Flags(kvImageNoFlags))
     }
 
-    func writeTexture(url: URL) {
+    func writeTexture(url: URL, type: UTType = .png) {
         guard let image = makeImage() else { return }
 
-        if let imageDestination = CGImageDestinationCreateWithURL(url as CFURL, UTType.png.identifier as CFString, 1, nil) {
+        if let imageDestination = CGImageDestinationCreateWithURL(url as CFURL, type.identifier as CFString, 1, nil) {
             CGImageDestinationAddImage(imageDestination, image, nil)
             CGImageDestinationFinalize(imageDestination)
         }
