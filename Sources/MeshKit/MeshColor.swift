dc6a39be8d8981d26aa8f21814a9ef7fb36772d4
Ethan Lipnik <hi@ethanlipnik.com>
2023-08-09 14:37:32 -0400

    fix macOS support

--- Sources/MeshKit/MeshColor.swift
+++ Sources/MeshKit/MeshColor.swift
@@ -8,7 +8,12 @@
 import CoreGraphics
 import Foundation
 import RandomColor
+
+#if canImport(UIKit)
 import UIKit
+#elseif canImport(AppKit)
+import AppKit
+#endif
 
 public class MeshColor: Equatable, Hashable, Codable {
     public static func == (lhs: MeshColor, rhs: MeshColor) -> Bool {
